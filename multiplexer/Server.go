22be8e02ab6e29a9cdb59f7dd7ddbcbfcb86075f
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-02-19 17:52:28 +0000

    added line to log remote address making request to server

--- multiplexer/Server.go
+++ multiplexer/Server.go
@@ -1,12 +1,14 @@
 package multiplexer
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/mrpiggy97/testingAwsBackend/middlewares"
+	"github.com/rs/zerolog/log"
 )
 
 type Server struct {
@@ -19,6 +21,8 @@ func (serverInstance *Server) ServeHTTP(writer http.ResponseWriter, request *htt
 	var allowedMethods string = strings.Join(serverInstance.allowedMethods, ",")
 	writer.Header().Add("Access-Control-Allow-Origin", serverInstance.allowedCrossSiteOrigin)
 	writer.Header().Add("Access-Control-Allow-Methods", allowedMethods)
+	message := fmt.Sprintf("server recieved request from remote address %v", request.RemoteAddr)
+	log.Info().Msg(message)
 	request = middlewares.ApplyMiddlewares(request)
 	serverInstance.router.ServeHTTP(writer, request)
 }
